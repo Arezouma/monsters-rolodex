cf3543eb44ade358ee18fe9efc6c8659b67bc7b3
Arezouma S Ngobou <sarezouma@gmail.com>
2019-11-08 11:34:31 -0500

    Updates

---
+++ precache-manifest.e77de3925123c23bbf0d42ab9eb620e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eea43af2c0dd30badc2a876534289b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8d2e9c0d5dbf25bceba6",
+    "url": "/monsters-rolodex/static/css/main.c5f4206c.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "8d2e9c0d5dbf25bceba6",
+    "url": "/monsters-rolodex/static/js/main.32f2aae0.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
